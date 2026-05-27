a296f9ac296396ac6dea74272e7a4eb53ef8f4a4
gmelo20 <seu-email@email.com>
2026-05-27 15:39:29 -0300

    Fix database connection for render

--- database.js
+++ database.js
@@ -1,15 +1,15 @@
-import pkg from 'pg';
+import pg from 'pg';
 import dotenv from 'dotenv';
 
 dotenv.config();
 
-const { Pool } = pkg;
+const { Pool } = pg;
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: false,
-  },
+    rejectUnauthorized: false
+  }
 });
 
 export default pool;
